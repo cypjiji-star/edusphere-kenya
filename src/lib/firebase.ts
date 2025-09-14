6f70e1c4d5e9b7f8f72d7b4cc6bc5a74caa83ac6
Derick <cypjiji@gmail.com>
2025-09-14 08:01:10 +0000

    am still getting an unknown error occured

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,10 +6,9 @@ import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
   "projectId": "studio-8322695498-589c5",
   "appId": "1:853801330881:web:025112dc4e29e784024c40",
-  "storageBucket": "studio-8322695498-589c5.firebasestorage.app",
+  "storageBucket": "studio-8322695498-589c5.appspot.com",
   "apiKey": "AIzaSyDxRU3vLJzXKFD5PpZ73eYxoI48pFveLNY",
   "authDomain": "studio-8322695498-589c5.firebaseapp.com",
-  "measurementId": "",
   "messagingSenderId": "853801330881"
 };
 
