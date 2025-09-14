661c05f3bac7f4706205508e456b14c502d1734a
Derick <cypjiji@gmail.com>
2025-09-14 11:39:42 +0000

    You are not authorized to view this page. Your role is listed as "Not Lo

--- src/app/developer/layout.tsx
+++ src/app/developer/layout.tsx
@@ -1,14 +1,24 @@
 
+'use client';
+
 import { SidebarProvider, Sidebar, SidebarInset } from '@/components/ui/sidebar';
 import { DeveloperSidebar } from './developer-sidebar';
 import { Suspense } from 'react';
 import { AuthCheck } from '@/lib/auth-check';
+import { usePathname } from 'next/navigation';
 
 export default function DeveloperLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+
+  // If the user is on the one-time creation page, don't run the auth check.
+  if (pathname === '/developer/create-dev-account') {
+    return <Suspense>{children}</Suspense>;
+  }
+
   return (
     <Suspense>
       <AuthCheck requiredRole="developer">
