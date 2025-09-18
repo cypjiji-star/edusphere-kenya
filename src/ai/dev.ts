cab4d4c9a8b6d7685f1cc7a1907ffa2bda1d3c18
Derick <cypjiji@gmail.com>
2025-09-18 23:19:12 +0000

    ./node_modules/dotprompt/dist/index.js ./node_modules/@genkit-ai/core/li

--- src/ai/dev.ts
+++
@@ -1,9 +0,0 @@
-
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/personalized-learning-path-generator.ts';
-import '@/ai/flows/lesson-plan-generator.ts';
-import '@/ai/flows/translate-text.ts';
-import '@/ai/flows/generate-team-icon-flow.ts';
-import '@/ai/flows/support-chatbot-flow.ts';
