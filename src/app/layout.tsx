65961cc9ece59614df73fc978767b57e62f132d8
jacinta adhiambo ogolla <cypjiji@gmail.com>
2025-09-21 14:03:15 +0000

    still light mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning className="dark">
+    <html lang="en" suppressHydrationWarning>
       <head>
         {/* Favicon */}
         <link rel="icon" href="/icon.svg" type="image/svg+xml" sizes="any" />
