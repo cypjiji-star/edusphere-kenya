e018af3c7302d5c5c6a06b19386ecd03fb2c3ce3
jacinta adhiambo ogolla <cypjiji@gmail.com>
2025-09-21 14:01:16 +0000

    even the login page should be in dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="dark">
       <head>
         {/* Favicon */}
         <link rel="icon" href="/icon.svg" type="image/svg+xml" sizes="any" />
