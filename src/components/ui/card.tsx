1a25f635546b6ab169b485983fcde2f359c442aa
jacinta adhiambo ogolla <cypjiji@gmail.com>
2025-09-21 12:22:51 +0000

    Subtle Gradients → Use pastel gradients for section headers or cards. in

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -28,7 +28,7 @@ const CardHeader = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex flex-col space-y-1.5 p-6", className)}
+    className={cn("flex flex-col space-y-1.5 p-6 bg-[var(--card-header-background)] rounded-t-lg", className)}
     {...props}
   />
 ))
