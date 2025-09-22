0dcc684e0bc9844aeeae8945c033a44e5a94ef24
jacinta adhiambo ogolla <cypjiji@gmail.com>
2025-09-22 18:13:44 +0000

    app/loading.tsx (global skeleton)

---
+++ src/app/loading.tsx
@@ -0,0 +1,13 @@
+
+'use client';
+
+import { Loader2 } from 'lucide-react';
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Loader2 className="h-10 w-10 animate-spin text-primary" />
+    </div>
+  );
+}
