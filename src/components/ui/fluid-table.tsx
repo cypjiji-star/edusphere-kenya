6da1fe43eb35bbac646803ac7fbfa19504fb6348
jacinta adhiambo ogolla <cypjiji@gmail.com>
2025-09-22 18:26:00 +0000

    components/ui/fluid-table.tsx

---
+++ src/components/ui/fluid-table.tsx
@@ -0,0 +1,118 @@
+
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+const FluidTable = React.forwardRef<
+  HTMLTableElement,
+  React.HTMLAttributes<HTMLTableElement>
+>(({ className, ...props }, ref) => (
+  <div className="relative w-full overflow-auto">
+    <table
+      ref={ref}
+      className={cn("w-full caption-bottom text-sm", className)}
+      {...props}
+    />
+  </div>
+))
+FluidTable.displayName = "FluidTable"
+
+const FluidTableHeader = React.forwardRef<
+  HTMLTableSectionElement,
+  React.HTMLAttributes<HTMLTableSectionElement>
+>(({ className, ...props }, ref) => (
+  <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props} />
+))
+FluidTableHeader.displayName = "FluidTableHeader"
+
+const FluidTableBody = React.forwardRef<
+  HTMLTableSectionElement,
+  React.HTMLAttributes<HTMLTableSectionElement>
+>(({ className, ...props }, ref) => (
+  <tbody
+    ref={ref}
+    className={cn("[&_tr:last-child]:border-0", className)}
+    {...props}
+  />
+))
+FluidTableBody.displayName = "FluidTableBody"
+
+const FluidTableFooter = React.forwardRef<
+  HTMLTableSectionElement,
+  React.HTMLAttributes<HTMLTableSectionElement>
+>(({ className, ...props }, ref) => (
+  <tfoot
+    ref={ref}
+    className={cn(
+      "border-t bg-muted/50 font-medium [&>tr]:last:border-b-0",
+      className
+    )}
+    {...props}
+  />
+))
+FluidTableFooter.displayName = "FluidTableFooter"
+
+const FluidTableRow = React.forwardRef<
+  HTMLTableRowElement,
+  React.HTMLAttributes<HTMLTableRowElement>
+>(({ className, ...props }, ref) => (
+  <tr
+    ref={ref}
+    className={cn(
+      "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
+      className
+    )}
+    {...props}
+  />
+))
+FluidTableRow.displayName = "FluidTableRow"
+
+const FluidTableHead = React.forwardRef<
+  HTMLTableCellElement,
+  React.ThHTMLAttributes<HTMLTableCellElement>
+>(({ className, ...props }, ref) => (
+  <th
+    ref={ref}
+    className={cn(
+      "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
+      className
+    )}
+    {...props}
+  />
+))
+FluidTableHead.displayName = "FluidTableHead"
+
+const FluidTableCell = React.forwardRef<
+  HTMLTableCellElement,
+  React.TdHTMLAttributes<HTMLTableCellElement>
+>(({ className, ...props }, ref) => (
+  <td
+    ref={ref}
+    className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
+    {...props}
+  />
+))
+FluidTableCell.displayName = "FluidTableCell"
+
+const FluidTableCaption = React.forwardRef<
+  HTMLTableCaptionElement,
+  React.HTMLAttributes<HTMLTableCaptionElement>
+>(({ className, ...props }, ref) => (
+  <caption
+    ref={ref}
+    className={cn("mt-4 text-sm text-muted-foreground", className)}
+    {...props}
+  />
+))
+FluidTableCaption.displayName = "FluidTableCaption"
+
+export {
+  FluidTable,
+  FluidTableHeader,
+  FluidTableBody,
+  FluidTableFooter,
+  FluidTableHead,
+  FluidTableRow,
+  FluidTableCell,
+  FluidTableCaption,
+}
