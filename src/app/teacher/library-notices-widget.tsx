f58d66ff3074ea8b9d974877a31a31cb3dc81da5
jacinta adhiambo ogolla <cypjiji@gmail.com>
2025-09-23 15:28:03 +0000

    check again on components

--- src/app/teacher/library-notices-widget.tsx
+++ src/app/teacher/library-notices-widget.tsx
@@ -55,7 +55,7 @@ export function LibraryNoticesWidget() {
         firestore,
         "schools",
         schoolId,
-        "teachers",
+        "users",
         teacherId,
         "borrowed-items",
       ),
